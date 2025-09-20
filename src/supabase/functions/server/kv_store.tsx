f16b7b9998530a61254277593a2d65d492265290
김민욱 <kmw1wlog@unist.ac.kr>
2025-09-20 10:46:48 +0900

    Add files via upload

--- src/supabase/functions/server/kv_store.tsx
+++ src/supabase/functions/server/kv_store.tsx
@@ -7,7 +7,7 @@ CREATE TABLE kv_store_3a80e39f (
 );
 */
 
-// View at https://supabase.com/dashboard/project/fycrkqrhrxcarllthqfl/database/tables
+// View at https://supabase.com/dashboard/project/pzyjcfkhdnczbfcpxjqb/database/tables
 
 // This file provides a simple key-value interface for storing Figma Make data. It should be adequate for most small-scale use cases.
 import { createClient } from "jsr:@supabase/supabase-js@2.49.8";
